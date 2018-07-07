fb96c5289c749ad842dcac4d6d203af1d3445a91
Sillas Teixeira Gonzaga <sillas.gonzaga@gmail.com>
2018-07-07 15:17:41 -0300

    first

---
+++ knit.R
@@ -0,0 +1,6 @@
+
+rmarkdown::render("post04_consumidor-pt1.Rmd", output_dir = "rendered", output_format = "all")
+rmarkdown::render("post05_consumidor-pt2.Rmd", output_dir = "rendered", output_format = "all")
+rmarkdown::render("post06_consumidor-pt3.Rmd", output_dir = "rendered", output_format = "all")
+
+rmarkdown::render("post07_consumidor-pt4.Rmd", output_dir = "rendered", output_format = "all")
